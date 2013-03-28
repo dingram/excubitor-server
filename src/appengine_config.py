5c566cafd4f81121be6d4badaede8be5e046895e
Dave Ingram <dave@dmi.me.uk>
2013-03-28 10:41:33 +0000

    Of course, the word "app" will conflict...

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,9 +1,9 @@
 appstats_CALC_RPC_COSTS = True
 appstats_TZOFFSET = 0
 
-def webapp_add_wsgi_middleware(app):
-	import app.config
-	if app.config.GAE_APP_STATS:
+def webapp_add_wsgi_middleware(wsgi_app):
+	import app.config as config
+	if config.GAE_APP_STATS:
 		from google.appengine.ext.appstats import recording
-		app = recording.appstats_wsgi_middleware(app)
-	return app
+		wsgi_app = recording.appstats_wsgi_middleware(wsgi_app)
+	return wsgi_app
