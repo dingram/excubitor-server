631c07872e0538cd6d11e7dc1e81709f4beef42f
Dave Ingram <dave@dmi.me.uk>
2013-03-28 10:20:32 +0000

    Initial templates

--- src/app/config.py
+++ src/app/config.py
@@ -1,9 +1,14 @@
 import os
 
-
+# Whether we are in debug mode, and whether we are on the local development server
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Development') or 'test' in os.environ.get('HTTP_HOST', os.environ.get('SERVER_NAME'))
 DEV_SERVER = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
 
+# Whether to record AppStats (annoying on the dev server)
+GAE_APP_STATS = not DEV_SERVER
+
+# Whether people are allowed to sign up to the service
 ALLOW_SIGNUPS = True
 
-GAE_APP_STATS = not DEV_SERVER
+# Google Analytics property ID (UA-xxxxxx-xx)
+ANALYTICS_ID = None
